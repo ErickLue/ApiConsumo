6350a95ea706a5f4b202ccc9560ca1a301bf0739
Ricardo Fuentes <bladimirfuentes29@gmail.com>
2024-09-27 14:10:01 -0600

    Agregar archivos de proyecto.

---
+++ ConsumoAPI/DTO/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsumoAPI.DTO
+{
+    public class UserSession
+    {
+        
+        public string Correo { get; set; } = null!;
+        public string Clave { get; set; } = null!;
+       
+    }
+}
---
+++ ConsumoAPI/Services/AuthService.cs
@@ -0,0 +1,87 @@
+﻿using ConsumoAPI.DTO;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ConsumoAPI.Services
+{
+    public class AuthService
+    {
+        private readonly ProtectedLocalStorage _localStorage;
+        private readonly HttpClient _httpClient;
+        private string? _token;
+
+        public AuthService(ProtectedLocalStorage localStorage, HttpClient httpClient)
+        {
+            _localStorage = localStorage;
+            _httpClient = httpClient;
+        }
+        //Enviar datos a enpoint Login 
+        public async Task<string> Login(UserSession userSession)
+        {
+            //usuarioSession.Correo = "chepe@gmail.com";
+            //usuarioSession.Clave = "12345";
+            var response = await _httpClient.PostAsJsonAsync("api/usuarios/Login", userSession);
+            if (response.IsSuccessStatusCode)
+            {
+                // Deserializar la respuesta JSON a un objeto LoginResponse
+                var result = await response.Content.ReadFromJsonAsync<string>();
+
+                // Verificar si el objeto no es nulo y devolver el token
+                return result;
+
+            }
+            return null;
+        }
+
+        //Guardar token en el navegador 
+        public async Task SetToken(string token)
+        {
+            _token = token;
+            await _localStorage.SetAsync("token", token);
+        }
+
+        //Obtener token del navegador 
+        public async Task<string> GetToken()
+        {
+
+            var localStorageResult = await _localStorage.GetAsync<string>("token");
+
+            if (string.IsNullOrEmpty(_token))
+            {
+
+                if (!localStorageResult.Success || string.IsNullOrEmpty(localStorageResult.Value))
+                {
+                    _token = null;
+                    return null;
+                }
+                _token = localStorageResult.Value;
+
+            }
+            return _token;
+        }
+
+        //Verificar si el usuario esta autenticado 
+        public async Task<bool> IsAuthenticated()
+        {
+            var token = await GetToken();
+
+            return !string.IsNullOrEmpty(token) && !IsTokenExpired(token);
+        }
+
+        //Verificar si el token a expirado
+
+        public bool IsTokenExpired(string token)
+        {
+            var jwtToken = new JwtSecurityToken(token);
+            return jwtToken.ValidTo < DateTime.UtcNow;
+        }
+        //cerrar sesion
+        public async Task Logout()
+        {
+            _token = null;
+            await _localStorage.DeleteAsync("token");
+        }
+
+
+    }
+}
